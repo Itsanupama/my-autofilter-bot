c735a082b0972af1c7a8f7f72533330dcdbe265b
Anupamafan <118895531+Anupamafan@users.noreply.github.com>
2023-03-18 12:29:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -711,8 +711,9 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Rᴇǫᴜᴇsᴛᴇᴅ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : <code>{search}</code>\n\n𝖎𝖋 𝖙𝖍𝖊 𝖒𝖔𝖛𝖎𝖊 ʏᴏᴜ ᴀʀᴇ ʀᴇǫᴜᴇsᴛᴇᴅ  ιѕ ησ𝔱 𝚂𝙷𝙾𝚆𝙸𝙽𝙶? \n\n₮ⱨɇ₦ ɽɇport Tø The  𝐀𝐃𝐌𝐈𝐍 @Anonymous_owner_bot"
-    if imdb and imdb.get('poster'):
+        cap = f"Rᴇǫᴜᴇsᴛᴇᴅ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : <code>{search}</code> (Please Check your Spelling On Google)\n\nUse Correct 𝗦𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴 Format :\n\nFor 𝗠𝗼𝘃𝗶𝗲👇\nSaaho 2019\n\nFor 𝗦𝗲𝗿𝗶𝗲𝘀\nMirzapur S01\n\n𝗗𝗼𝗻'𝘁 𝗨𝘀𝗲 𝗢𝘁𝗵𝗲𝗿 𝗪𝗼𝗿𝗱𝘀 𝗟𝗶𝗸𝗲 ❌ 👇 \nMovie, Series, Hindi Movie, South Movie, Dubbed, Please Send, Full HD Movie, All Seasons, Full series, etc.....! /n/nAny help? Contact me @Anonymous_owner_bot
+
+        if imdb and imdb.get('poster'):
         try:
             hehe =  await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
                                       reply_markup=InlineKeyboardMarkup(btn))
