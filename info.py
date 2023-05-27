583d26068bad1f401a7cf114dfae628e3f55ff4f
Anupamafan <118895531+Anupamafan@users.noreply.github.com>
2023-05-27 12:53:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
       # URL Shortener #
 
 URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'tinyfy.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '4fd3b2330524432b7688b9ada2153c4b932b02d2')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 8700))
